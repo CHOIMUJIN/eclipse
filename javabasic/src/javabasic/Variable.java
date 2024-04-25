6ef9a98f03b8d121921e5e3d66d9de8d18e697c6
Administrator <Administrator@DESKTOP-HUJUVSS>
2024-04-25 17:27:43 +0900

    uiyuyiyuiu

---
+++ javabasic/src/javabasic/Variable.java
@@ -0,0 +1,26 @@
+package javabasic;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		byte b = 100;
+		short s = 30000;
+		int i = 2100000000;
+		long l = 2134712038912L;
+		float f = 100.1f;
+		double d = 1237891243.2343;
+		boolean bool = true;
+		char ch = '가';
+		
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(bool);
+		System.out.println('가');
+	}
+
+}
